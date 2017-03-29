3fde1595c4301be45979ca2a1b51d456c79efb07
songjiaqi007 <609208788@qq.com>
2017-03-29 12:02:53 +0800

    添加欢迎界面

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,8 +1,17 @@
 package cn.ucai.superwechat.ui;
 
+import android.os.Bundle;
+
+import cn.ucai.superwechat.R;
+
 /**
  * Created by liuning on 2017/3/29.
  */
 
-public class GuideActivity {
+public class GuideActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.activity_guide);
+    }
 }
