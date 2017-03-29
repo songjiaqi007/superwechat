081dfbad1ccf5e9dc8b4b347030c8cb58b4ad1e7
songjiaqi007 <609208788@qq.com>
2017-03-29 11:52:15 +0800

    添加跳转工具类，修改闪屏跳转到主页面或者欢迎界面

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -0,0 +1,8 @@
+package cn.ucai.superwechat.ui;
+
+/**
+ * Created by liuning on 2017/3/29.
+ */
+
+public class GuideActivity {
+}
--- ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
@@ -1,20 +1,42 @@
 package cn.ucai.superwechat.utils;
 
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.GuideActivity;
+import cn.ucai.superwechat.ui.MainActivity;
+
 /**
  * Created by liuning on 2017/3/16.
  */
 
 public class MFGT {
-    /*public static void finish(Activity activity) {
+    public static void finish(Activity activity) {
         activity.finish();
         activity.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
     }
 
+    public static void starActivity(Activity activity, Class cls) {
+        activity.startActivity(new Intent(activity,cls));
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+
+    public static void statActivity(Activity activity, Intent intent) {
+        activity.startActivity(intent);
+        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+    }
+
+    public static void gotoMain(Activity activity) {
+        starActivity(activity, MainActivity.class);
+    }
 
     public static void startActivityForResult(Activity activity, Intent intent, int requestCode) {
         activity.startActivityForResult(intent,requestCode);
         activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
     }
-*/
 
+    public static void gotoGuide(Activity activity) {
+       starActivity(activity, GuideActivity.class);
+    }
 }
