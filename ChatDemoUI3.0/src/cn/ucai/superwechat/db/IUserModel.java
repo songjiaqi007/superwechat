86a938671528379140c042e41293212981b7c5ba
songjiaqi007 <609208788@qq.com>
2017-04-04 20:47:54 +0800

    更新用户昵称

--- ChatDemoUI3.0/src/cn/ucai/superwechat/db/IUserModel.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/db/IUserModel.java
@@ -2,22 +2,19 @@ package cn.ucai.superwechat.db;
 
 import android.content.Context;
 
-import cn.ucai.superwechat.utils.OkHttpUtils;
+import java.io.File;
 
 /**
- * Created by liuning on 2017/3/29.
+ * Created by clawpo on 2017/3/29.
  */
 
 public interface IUserModel {
-    void register(Context context, String username, String nickname, String password,
-                  OkHttpUtils.OnCompleteListener<String> listener);
-
-    void login(Context context, String username, String password,
-               OkHttpUtils.OnCompleteListener<String> listener);
-
-    void unregister(Context context, String username,
-                    OkHttpUtils.OnCompleteListener<String> listener);
-
+    void register(Context context,String username,String nickname,String password,
+                  OnCompleteListener<String> listener);
+    void login(Context context,String username,String password,
+               OnCompleteListener<String> listener);
+    void unregister(Context context,String username,OnCompleteListener<String> listener);
     void loadUserInfo(Context context,String username,OnCompleteListener<String> listener);
-
+    void updateUserNick(Context context,String username,String nickname,OnCompleteListener<String> listener);
+    void uploadAvatar(Context context,String username,File file,OnCompleteListener<String> listener);
 }
--- ChatDemoUI3.0/src/cn/ucai/superwechat/db/UserModel.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/db/UserModel.java
@@ -2,17 +2,18 @@ package cn.ucai.superwechat.db;
 
 import android.content.Context;
 
+import java.io.File;
+
 import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.utils.OkHttpUtils;
 
-
 /**
- * Created by liuning on 2017/3/29.
+ * Created by clawpo on 2017/3/29.
  */
 
 public class UserModel implements IUserModel {
     @Override
-    public void register(Context context, String username, String nickname, String password, OkHttpUtils.OnCompleteListener<String> listener) {
+    public void register(Context context, String username, String nickname, String password, OnCompleteListener<String> listener) {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
         utils.setRequestUrl(I.REQUEST_REGISTER)
                 .addParam(I.User.USER_NAME,username)
@@ -21,26 +22,25 @@ public class UserModel implements IUserModel {
                 .post()
                 .targetClass(String.class)
                 .execute(listener);
-
     }
 
     @Override
-    public void login(Context context, String username, String password, OkHttpUtils.OnCompleteListener<String> listener) {
+    public void login(Context context, String username, String password, OnCompleteListener<String> listener) {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
-                utils.setRequestUrl(I.REQUEST_LOGIN)
-                        .addParam(I.User.USER_NAME,username)
-                        .addParam(I.User.PASSWORD,password)
-                        .targetClass(String.class)
-                        .execute(listener);
+        utils.setRequestUrl(I.REQUEST_LOGIN)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.PASSWORD,password)
+                .targetClass(String.class)
+                .execute(listener);
     }
 
     @Override
-    public void unregister(Context context, String username, OkHttpUtils.OnCompleteListener<String> listener) {
+    public void unregister(Context context, String username, OnCompleteListener<String> listener) {
         OkHttpUtils<String> utils = new OkHttpUtils<>(context);
-                utils.setRequestUrl(I.REQUEST_UNREGISTER)
-                        .addParam(I.User.USER_NAME,username)
-                        .targetClass(String.class)
-                        .execute(listener);
+        utils.setRequestUrl(I.REQUEST_UNREGISTER)
+                .addParam(I.User.USER_NAME,username)
+                .targetClass(String.class)
+                .execute(listener);
     }
 
     @Override
@@ -49,6 +49,29 @@ public class UserModel implements IUserModel {
         utils.setRequestUrl(I.REQUEST_FIND_USER)
                 .addParam(I.User.USER_NAME,username)
                 .targetClass(String.class)
+                .execute(listener);
+    }
+
+    @Override
+    public void updateUserNick(Context context, String username, String nickname, OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_USER_NICK)
+                .addParam(I.User.USER_NAME,username)
+                .addParam(I.User.NICK,nickname)
+                .targetClass(String.class)
+                .execute(listener);
+    }
+
+    @Override
+    public void uploadAvatar(Context context, String username, File file,
+                             OnCompleteListener<String> listener) {
+        OkHttpUtils<String> utils = new OkHttpUtils<>(context);
+        utils.setRequestUrl(I.REQUEST_UPDATE_AVATAR)
+                .addParam(I.NAME_OR_HXID,username)
+                .addParam(I.AVATAR_TYPE,I.AVATAR_TYPE_USER_PATH)
+                .addFile2(file)
+                .targetClass(String.class)
+                .post()
                 .execute(listener);
     }
 }
