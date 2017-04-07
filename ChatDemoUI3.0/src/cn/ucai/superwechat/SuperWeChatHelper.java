23d6ef87d20fdd1a57774e29cc83783b96bfbbf9
songjiaqi007 <609208788@qq.com>
2017-04-07 15:09:30 +0800

    切换用户登录，重新从服务器加载用户列表

--- ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatHelper.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/SuperWeChatHelper.java
@@ -1266,6 +1266,8 @@ public class SuperWeChatHelper {
 
         isSyncingContactsWithServer = true;
 
+        asyncFetchAppContactsFromServer();
+
         new Thread(){
             @Override
             public void run(){
@@ -1331,6 +1333,41 @@ public class SuperWeChatHelper {
 
             }
         }.start();
+    }
+
+    public void asyncFetchAppContactsFromServer() {
+        if (isLoggedIn()) {
+            userModel.loadContact(appContext, EMClient.getInstance().getCurrentUser(),
+                    new OnCompleteListener<String>() {
+                        @Override
+                        public void onSuccess(String s) {
+                            if (s!=null) {
+                                Result result = ResultUtils.getListResultFromJson(s, I.User.class);
+                                if (result!=null && result.isRetMsg()) {
+                                    List<User> list = (List<User>) result.getRetData();
+                                    Map<String, User> userlist = new HashMap<String, User>();
+                                    for (User user : list) {
+                                        EaseCommonUtils.setAppUserInitialLetter(user);
+                                        userlist.put(user.getMUserName(), user);
+                                    }
+                                    // save the contact list to cache
+                                    getAppContactList().clear();
+                                    getAppContactList().putAll(userlist);
+                                    // save the contact list to database
+                                    UserDao dao = new UserDao(appContext);
+                                    List<User> users = new ArrayList<User>(userlist.values());
+                                    dao.saveAppContactList(users);
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onError(String error) {
+
+                        }
+                    });
+        }
+
     }
 
     public void notifyContactsSyncListener(boolean success){
@@ -1432,6 +1469,7 @@ public class SuperWeChatHelper {
         isGroupAndContactListenerRegisted = false;
 
         setContactList(null);
+        setAppContactList(null);
         setRobotList(null);
         getUserProfileManager().reset();
         SuperWeChatDBManager.getInstance().closeDB();
