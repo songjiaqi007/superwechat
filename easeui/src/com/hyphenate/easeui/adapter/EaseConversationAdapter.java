d3941b63b6cc5dc5b85f0f2a5783a6cc604977f1
songjiaqi007 <609208788@qq.com>
2017-04-11 06:52:39 +0800

    会话列表增加用户名和昵称的模糊搜索

--- easeui/src/com/hyphenate/easeui/adapter/EaseConversationAdapter.java
+++ easeui/src/com/hyphenate/easeui/adapter/EaseConversationAdapter.java
@@ -34,6 +34,7 @@ import java.util.List;
 
 /**
  * conversation list adapter
+ *
  */
 public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
     private static final String TAG = "ChatAllHistoryAdapter";
@@ -48,7 +49,6 @@ public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
     protected int primarySize;
     protected int secondarySize;
     protected float timeSize;
-    String nickname;
 
     public EaseConversationAdapter(Context context, int resource,
                                    List<EMConversation> objects) {
@@ -103,21 +103,21 @@ public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
 
         if (conversation.getType() == EMConversationType.GroupChat) {
             String groupId = conversation.conversationId();
-            if (EaseAtMessageHelper.get().hasAtMeMsg(groupId)) {
+            if(EaseAtMessageHelper.get().hasAtMeMsg(groupId)){
                 holder.motioned.setVisibility(View.VISIBLE);
-            } else {
+            }else{
                 holder.motioned.setVisibility(View.GONE);
             }
             // group message, show group avatar
             holder.avatar.setImageResource(R.drawable.ease_group_icon);
             EMGroup group = EMClient.getInstance().groupManager().getGroup(username);
             holder.name.setText(group != null ? group.getGroupName() : username);
-        } else if (conversation.getType() == EMConversationType.ChatRoom) {
+        } else if(conversation.getType() == EMConversationType.ChatRoom){
             holder.avatar.setImageResource(R.drawable.ease_group_icon);
             EMChatRoom room = EMClient.getInstance().chatroomManager().getChatRoom(username);
             holder.name.setText(room != null && !TextUtils.isEmpty(room.getName()) ? room.getName() : username);
             holder.motioned.setVisibility(View.GONE);
-        } else {
+        }else {
             EaseUserUtils.setAppUserAvatar(getContext(), username, holder.avatar);
             EaseUserUtils.setAppUserNick(username, holder.name);
             holder.motioned.setVisibility(View.GONE);
@@ -135,12 +135,12 @@ public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
             // show the content of latest message
             EMMessage lastMessage = conversation.getLastMessage();
             String content = null;
-            if (cvsListHelper != null) {
+            if(cvsListHelper != null){
                 content = cvsListHelper.onSetItemSecondaryText(lastMessage);
             }
             holder.message.setText(EaseSmileUtils.getSmiledText(getContext(), EaseCommonUtils.getMessageDigest(lastMessage, (this.getContext()))),
                     BufferType.SPANNABLE);
-            if (content != null) {
+            if(content != null){
                 holder.message.setText(content);
             }
             holder.time.setText(DateUtils.getTimestampString(new Date(lastMessage.getMsgTime())));
@@ -155,11 +155,11 @@ public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
         holder.name.setTextColor(primaryColor);
         holder.message.setTextColor(secondaryColor);
         holder.time.setTextColor(timeColor);
-        if (primarySize != 0)
+        if(primarySize != 0)
             holder.name.setTextSize(TypedValue.COMPLEX_UNIT_PX, primarySize);
-        if (secondarySize != 0)
+        if(secondarySize != 0)
             holder.message.setTextSize(TypedValue.COMPLEX_UNIT_PX, secondarySize);
-        if (timeSize != 0)
+        if(timeSize != 0)
             holder.time.setTextSize(TypedValue.COMPLEX_UNIT_PX, timeSize);
 
         return convertView;
@@ -168,7 +168,7 @@ public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
     @Override
     public void notifyDataSetChanged() {
         super.notifyDataSetChanged();
-        if (!notiyfyByFilter) {
+        if(!notiyfyByFilter){
             copyConversationList.clear();
             copyConversationList.addAll(conversationList);
             notiyfyByFilter = false;
@@ -227,30 +227,29 @@ public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
                 results.values = copyConversationList;
                 results.count = copyConversationList.size();
             } else {
-                String prefixString = prefix.toString();
+                String prefixString = prefix.toString().toLowerCase();
                 final int count = mOriginalValues.size();
                 final ArrayList<EMConversation> newValues = new ArrayList<EMConversation>();
 
                 for (int i = 0; i < count; i++) {
                     final EMConversation value = mOriginalValues.get(i);
-                    String username = value.conversationId();
+                    String username = value.conversationId().toLowerCase();
+                    String nickname = "";
 
                     EMGroup group = EMClient.getInstance().groupManager().getGroup(username);
-                    if (group != null) {
+                    if(group != null){
                         username = group.getGroupName();
-                    } else {
+                    }else{
                         User user = EaseUserUtils.getAppUserInfo(username);
                         // TODO: not support Nick anymore
-//                        if(user != null && user.getNick() != null)
-//                            username = user.getNick();
-                        username = user.getMUserName().toLowerCase();
-                        nickname = user.getMUserNick().toLowerCase();
+                        if(user != null && user.getMUserNick() != null)
+                            nickname = user.getMUserNick().toLowerCase();
                     }
 
                     // First match against the whole ,non-splitted value
                     if (username.contains(prefixString) || nickname.contains(prefixString)) {
                         newValues.add(value);
-                    } else {
+                    } else{
                         final String[] words = username.split(" ");
                         final int wordCount = words.length;
 
@@ -287,40 +286,25 @@ public class EaseConversationAdapter extends ArrayAdapter<EMConversation> {
 
     private EaseConversationListHelper cvsListHelper;
 
-    public void setCvsListHelper(EaseConversationListHelper cvsListHelper) {
+    public void setCvsListHelper(EaseConversationListHelper cvsListHelper){
         this.cvsListHelper = cvsListHelper;
     }
 
     private static class ViewHolder {
-        /**
-         * who you chat with
-         */
+        /** who you chat with */
         TextView name;
-        /**
-         * unread message count
-         */
+        /** unread message count */
         TextView unreadLabel;
-        /**
-         * content of last message
-         */
+        /** content of last message */
         TextView message;
-        /**
-         * time of last message
-         */
+        /** time of last message */
         TextView time;
-        /**
-         * avatar
-         */
+        /** avatar */
         ImageView avatar;
-        /**
-         * status of last message
-         */
+        /** status of last message */
         View msgState;
-        /**
-         * layout
-         */
+        /** layout */
         RelativeLayout list_itease_layout;
         TextView motioned;
     }
 }
-
