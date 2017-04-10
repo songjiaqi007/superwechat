3703b9e2d6019d9fd23633e45ba1959e3d233157
songjiaqi007 <609208788@qq.com>
2017-04-11 07:12:25 +0800

    创建群组的数据接口及实现

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/db/IGroupModel.java
@@ -0,0 +1,14 @@
+package cn.ucai.superwechat.db;
+
+import android.content.Context;
+
+import java.io.File;
+
+/**
+ * Created by clawpo on 2017/3/29.
+ */
+
+public interface IGroupModel {
+    void newGroup(Context context, String hxid,String groupName,String des,String owner,boolean isPublic,
+                  boolean isInvites,File file,OnCompleteListener<String> listener);
+}
