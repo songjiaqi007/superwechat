c88b7eacbe4df628a4332ea6e338060dd77da022
songjiaqi007 <609208788@qq.com>
2017-04-10 12:36:40 +0800

    删除联系人

--- ChatDemoUI3.0/src/cn/ucai/superwechat/db/IUserModel.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/db/IUserModel.java
@@ -19,4 +19,5 @@ public interface IUserModel {
     void uploadAvatar(Context context,String username,File file,OnCompleteListener<String> listener);
     void addContact(Context context,String username,String cname, OnCompleteListener<String> listener);
     void loadContact(Context context,String username,OnCompleteListener<String> listener);
+    void delContact(Context context,String username,String cname, OnCompleteListener<String> listener);
 }
