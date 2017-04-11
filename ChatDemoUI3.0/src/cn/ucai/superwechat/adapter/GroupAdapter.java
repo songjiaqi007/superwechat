dd35fe43d233e0628d1447e945cca0cd6fb35f83
songjiaqi007 <609208788@qq.com>
2017-04-11 20:01:48 +0800

    群聊列表里显示群组的图标

--- ChatDemoUI3.0/src/cn/ucai/superwechat/adapter/GroupAdapter.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/adapter/GroupAdapter.java
@@ -27,99 +27,104 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.hyphenate.chat.EMGroup;
-import cn.ucai.superwechat.R;
+import com.hyphenate.easeui.domain.Group;
+import com.hyphenate.easeui.utils.EaseUserUtils;
 
 import java.util.List;
 
+import cn.ucai.superwechat.R;
+
 public class GroupAdapter extends ArrayAdapter<EMGroup> {
+    private static final String TAG = "GroupAdapter";
 
-	private LayoutInflater inflater;
-	private String newGroup;
-	private String addPublicGroup;
+    private LayoutInflater inflater;
+    private String newGroup;
+    private String addPublicGroup;
 
-	public GroupAdapter(Context context, int res, List<EMGroup> groups) {
-		super(context, res, groups);
-		this.inflater = LayoutInflater.from(context);
-		newGroup = context.getResources().getString(R.string.The_new_group_chat);
-		addPublicGroup = context.getResources().getString(R.string.add_public_group_chat);
-	}
+    public GroupAdapter(Context context, int res, List<EMGroup> groups) {
+        super(context, res, groups);
+        this.inflater = LayoutInflater.from(context);
+        newGroup = context.getResources().getString(R.string.The_new_group_chat);
+        addPublicGroup = context.getResources().getString(R.string.add_public_group_chat);
+    }
 
-	@Override
-	public int getViewTypeCount() {
-		return 4;
-	}
+    @Override
+    public int getViewTypeCount() {
+        return 4;
+    }
 
-	@Override
-	public int getItemViewType(int position) {
-		if (position == 0) {
-			return 0;
-		} else if (position == 1) {
-			return 1;
-		} else if (position == 2) {
-			return 2;
-		} else {
-			return 3;
-		}
-	}
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return 0;
+        } else if (position == 1) {
+            return 1;
+        } else if (position == 2) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		if (getItemViewType(position) == 0) {
-			if (convertView == null) {
-				convertView = inflater.inflate(R.layout.em_search_bar_with_padding, parent, false);
-			}
-			final EditText query = (EditText) convertView.findViewById(R.id.query);
-			final ImageButton clearSearch = (ImageButton) convertView.findViewById(R.id.search_clear);
-			query.addTextChangedListener(new TextWatcher() {
-				public void onTextChanged(CharSequence s, int start, int before, int count) {
-					getFilter().filter(s);
-					if (s.length() > 0) {
-						clearSearch.setVisibility(View.VISIBLE);
-					} else {
-						clearSearch.setVisibility(View.INVISIBLE);
-					}
-				}
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (getItemViewType(position) == 0) {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.em_search_bar_with_padding, parent, false);
+            }
+            final EditText query = (EditText) convertView.findViewById(R.id.query);
+            final ImageButton clearSearch = (ImageButton) convertView.findViewById(R.id.search_clear);
+            query.addTextChangedListener(new TextWatcher() {
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    getFilter().filter(s);
+                    if (s.length() > 0) {
+                        clearSearch.setVisibility(View.VISIBLE);
+                    } else {
+                        clearSearch.setVisibility(View.INVISIBLE);
+                    }
+                }
 
-				public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-				}
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
 
-				public void afterTextChanged(Editable s) {
-				}
-			});
-			clearSearch.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					query.getText().clear();
-				}
-			});
-		} else if (getItemViewType(position) == 1) {
-			if (convertView == null) {
-				convertView = inflater.inflate(R.layout.em_row_add_group, parent, false);
-			}
-			((ImageView) convertView.findViewById(R.id.avatar)).setImageResource(R.drawable.em_create_group);
-			((TextView) convertView.findViewById(R.id.name)).setText(newGroup);
-		} else if (getItemViewType(position) == 2) {
-			if (convertView == null) {
-				convertView = inflater.inflate(R.layout.em_row_add_group, parent, false);
-			}
-			((ImageView) convertView.findViewById(R.id.avatar)).setImageResource(R.drawable.em_add_public_group);
-			((TextView) convertView.findViewById(R.id.name)).setText(addPublicGroup);
-			((TextView) convertView.findViewById(R.id.header)).setVisibility(View.VISIBLE);
+                public void afterTextChanged(Editable s) {
+                }
+            });
+            clearSearch.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    query.getText().clear();
+                }
+            });
+        } else if (getItemViewType(position) == 1) {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.em_row_add_group, parent, false);
+            }
+            ((ImageView) convertView.findViewById(R.id.avatar)).setImageResource(R.drawable.em_create_group);
+            ((TextView) convertView.findViewById(R.id.name)).setText(newGroup);
+        } else if (getItemViewType(position) == 2) {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.em_row_add_group, parent, false);
+            }
+            ((ImageView) convertView.findViewById(R.id.avatar)).setImageResource(R.drawable.em_add_public_group);
+            ((TextView) convertView.findViewById(R.id.name)).setText(addPublicGroup);
+            ((TextView) convertView.findViewById(R.id.header)).setVisibility(View.VISIBLE);
 
-		} else {
-			if (convertView == null) {
-				convertView = inflater.inflate(R.layout.em_row_group, parent, false);
-			}
-			((TextView) convertView.findViewById(R.id.name)).setText(getItem(position - 3).getGroupName());
+        } else {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.em_row_group, parent, false);
+            }
+            ((TextView) convertView.findViewById(R.id.name)).setText(getItem(position - 3).getGroupName());
+            EaseUserUtils.setGroupAvatar(getContext(), Group.getAvatar(getItem(position - 3).getGroupId()),
+                    ((ImageView) convertView.findViewById(R.id.avatar)));
+        }
 
-		}
+        return convertView;
+    }
 
-		return convertView;
-	}
-
-	@Override
-	public int getCount() {
-		return super.getCount() + 3;
-	}
+    @Override
+    public int getCount() {
+        return super.getCount() + 3;
+    }
 
 }
--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -108,6 +108,20 @@ public class EaseUserUtils {
             Glide.with(context).load(R.drawable.default_hd_avatar).into(imageView);
         }
     }
+    public static void setGroupAvatar(Context context,String avatarPath, ImageView imageView){
+        if(avatarPath != null){
+            try {
+                int avatarResId = Integer.parseInt(avatarPath);
+                Glide.with(context).load(avatarResId).into(imageView);
+            } catch (Exception e) {
+                //use default avatar
+                Glide.with(context).load(avatarPath).diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .placeholder(R.drawable.ease_group_icon).into(imageView);
+            }
+        }else{
+            Glide.with(context).load(R.drawable.ease_group_icon).into(imageView);
+        }
+    }
 
     /**
      * set user's nickname
